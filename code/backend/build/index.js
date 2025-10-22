25091acf2ce72b77eded1afb3bba4b45ca672011
Andy Del Real <afdelreal04@gmail.com>
2025-10-22 15:24:45 -0500

    Added dev script

--- code/backend/build/index.js
+++ code/backend/build/index.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello World!');
 });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
