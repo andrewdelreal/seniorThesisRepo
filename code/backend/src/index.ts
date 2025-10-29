bc72d382abc8dbdcfecee6936f6eff357365c004
Andy Del Real <afdelreal04@gmail.com>
2025-10-29 09:02:09 -0500

    Fixed TypeScript

--- code/backend/src/index.ts
+++ code/backend/src/index.ts
@@ -1,52 +1,52 @@
-import express from 'express';
+import express, { Application } from 'express';
 import path from 'path';
 import DBAbstraction from './DBAbstraction';
 import cors from 'cors';
 import morgan from 'morgan';
 import jwt from "jsonwebtoken";
-import { OAuth2Client } from "google-auth-library";
+import { LoginTicket, OAuth2Client, TokenPayload } from "google-auth-library";
 import { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-const app = express();
+const app: Application = express();
 
-const PORT = 3000;
+const PORT: number = 3000;
 
-const dbPath = path.join(__dirname, '..', 'data', 'database.db');
-const db = new DBAbstraction(dbPath);
+const dbPath: string = path.join(__dirname, '..', 'data', 'database.db');
+const db: DBAbstraction = new DBAbstraction(dbPath);
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'))
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+const GOOGLE_CLIENT_ID: string = process.env.GOOGLE_CLIENT_ID!;
+const client: OAuth2Client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
 const APP_JWT_SECRET = process.env.APP_JWT_SECRET!;
 
 // Verify Google token
 async function verifyGoogleToken(token: string) {
-  const ticket = await client.verifyIdToken({ // verify credentials with Google
+  const ticket: LoginTicket = await client.verifyIdToken({ // verify credentials with Google
     idToken: token,
     audience: GOOGLE_CLIENT_ID,
   });
   return ticket.getPayload();
 }
 
-app.post("/api/auth/google", async (req, res) => {
+app.post("/api/auth/google", async (req: Request<{}, {}, {token: string}>, res: Response) => {
   const { token } = req.body;
 
   try {
-    const payload = await verifyGoogleToken(token); // Google verigies credentials
+    const payload: TokenPayload | undefined = await verifyGoogleToken(token); // Google verigies credentials
     if (!payload) return res.status(401).json({ error: "Invalid token" });
 
-    const { sub } = payload; // Google’s unique user ID
+    const sub: string = payload.sub; // Google’s unique user ID
     if (!sub) return res.status(400).json({ error: "Missing user ID" });
 
-    const appToken = jwt.sign({ googleId: sub }, APP_JWT_SECRET, { expiresIn: "7d" });  // create token valid for 7 days
+    const appToken: string = jwt.sign({ googleId: sub }, APP_JWT_SECRET, { expiresIn: "7d" });  // create token valid for 7 days
 
     // Send user token that is valid for 7days
     res.json({ appToken });
@@ -56,16 +56,16 @@ app.post("/api/auth/google", async (req, res) => {
   }
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
 //middleware for pretected routes
 function authenticate(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers.authorization;
+  const authHeader: string | undefined = req.headers.authorization;
   if (!authHeader) return res.status(401).json({ error: "Missing token" });
 
-  const token = authHeader.split(" ")[1];
+  const token: string = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, APP_JWT_SECRET) as { googleId: string };
     (req as any).googleId = decoded.googleId;
