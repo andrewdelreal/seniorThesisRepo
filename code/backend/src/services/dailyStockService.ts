68a7e952b417e011c4aeea79e233f1d967259193
andrewdelreal <afdelreal04@gmail.com>
2026-04-16 15:47:07 -0500

    Fixed market time comparison to run stock update

--- code/backend/src/services/dailyStockService.ts
+++ code/backend/src/services/dailyStockService.ts
@@ -20,7 +20,7 @@ export async function DailyStockUpdate() {
     const now = new Date();
 
     const estNow = new Date(
-        now.toLocaleString("en-CA", { timeZone: "America/New_York" })
+        now.toLocaleString("en-US", { timeZone: "America/New_York" })
     );
 
     const marketCloseTime = new Date(estNow);
